754070682ea3435f7922abb9e00bd8d9ae326a5d
Bencho <ruben.camilo321@gmail.com>
2026-09-10 13:58:33 -0500

    feat: initialize siesa-integration-service dependencies, update services, and remove deprecated configuration files

--- services/products-service/dist/utils/asyncContext.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const async_hooks_1 = require("async_hooks");
-const asyncContext = new async_hooks_1.AsyncLocalStorage();
-exports.default = asyncContext;
--- services/stores-service/dist/utils/asyncContext.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncContext = void 0;
-const async_hooks_1 = require("async_hooks");
-exports.asyncContext = new async_hooks_1.AsyncLocalStorage();
